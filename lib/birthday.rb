9552f2b3bd5974d921d1f51ae8c36378daae772a
seth <seth623@gmail.com>
2022-01-05 17:53:15 -0700

    Completed Assignmnent

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,11 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
-end
+  
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!" 
+  end 
+
+end 
 
 
